539fba2be6fe2480cd4281b0556835a12afe8cf1
marcivallario <68357009+marcivallario@users.noreply.github.com>
2022-04-26 22:03:38 -0400

    Trips In Progress Dashboard Widget Complete
    - add TripsInProgress.js component
    - link to styles from Active Projects widget
    - cleanup styles in dashboard.css

--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -5,6 +5,7 @@ import ActiveProjects from './components/ActiveProjects';
 import CompletedTrips from './components/CompletedTrips';
 import TotalProjects from './components/TotalProjects';
 import TotalPassengers from './components/TotalPassengers';
+import TripsInProgress from './components/TripsInProgress';
 import './dashboard.css';
 
 let items = [
@@ -16,12 +17,12 @@ let items = [
 
 function Dashboard() {
     return (
-        <div className="dashboard">
+        <div id="dashboard">
             <div className="row">
                 <div className="col-12 col-xl-6">
                     <div id="upcoming-flights">
                         <div className="cardheader">
-                            <h3>Upcoming Flights</h3>
+                            <h3>Flights Within 24 Hours</h3>
                         </div>
                         <div id="boarding-wrapper">
                             <AliceCarousel 
@@ -42,7 +43,7 @@ function Dashboard() {
                             <h3>Active Projects</h3>
                             <a className="see-all" href="/projects">See all</a>
                         </div>
-                        <div id="activeprojects-wrapper">
+                        <div id="activeprojects-wrapper" className="dash-widget">
                             <ActiveProjects />
                         </div>
                     </div>
@@ -59,12 +60,20 @@ function Dashboard() {
                     <TotalPassengers />
                 </div>
             </div>
-            <div className="row">
-                <div className="col">
-                    <Card>Map</Card>
+            <div className="row" id="current-trips">
+                <div className="col-12 col-xl-6">
+                    <div id="trips-in-progress">
+                        <div className="cardheader">
+                            <h3>Trips In Progress</h3>
+                            <a className="see-all" href="/trips">See all</a>
+                        </div>
+                        <div id="currenttrips-wrapper" className="dash-widget">
+                            <TripsInProgress />
+                        </div>
+                    </div>
                 </div>
                 <div className="col">
-                    <Card>Recent Trips</Card>
+                    <Card>Current Destinations</Card>
                 </div>
             </div>
         </div>
