8e6d22981593cae2ff4d1b5f3121c106f114af15
marcivallario <68357009+marcivallario@users.noreply.github.com>
2022-05-02 16:58:28 -0400

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# reset database: rails db:reset db:seed
+
+puts "Seeding..."
+
+user1 = User.create!(email: 'marcivallario@gmail.com', first_name: 'Marci', last_name: 'Vallario', password: 'Marci123')
+
+puts "Seeding complete!"
