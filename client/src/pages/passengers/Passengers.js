1c68928b853338a27d2884d998804b0fa219251e
marcivallario <68357009+marcivallario@users.noreply.github.com>
2022-05-02 16:10:33 -0400

    Passengers List Complete
    - add passenger page component
    - add passenger list styles
    - cleanup style added in navigation.css

--- client/src/pages/passengers/Passengers.js
+++ client/src/pages/passengers/Passengers.js
@@ -1,6 +1,96 @@
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Card from '@mui/material/Card';
+import './passengers.css'
+
 function Passengers() {
     return (
-        <div>This is the Passengers component.</div>
+        <div id="passengers">
+            <div className="table-header">
+                 <h1>Passengers</h1>
+                <input></input>
+            </div>
+            <TableContainer 
+            component={Card}
+            sx={{ 
+                padding: "10px",
+                overflow: "scroll",
+                boxShadow: "0px 0px 25px 8px rgba(0,0,0,0.1)",
+                borderRadius: "12px",
+                padding: "20px"
+            }}
+        >
+            <Table 
+                sx={{ 
+                    overflow: "scroll",
+                    borderSpacing: "0 5px",
+                    borderCollapse: "separate"
+                }} 
+                size="small" 
+            >
+                <TableHead sx={{}}>
+                    <TableRow>
+                        <TableCell sx={{border: "none", fontWeight: "700"}}>Name</TableCell>
+                        <TableCell sx={{
+                            border: "none", 
+                            fontWeight: "700",
+                            display: { xs: 'none', sm: 'table-cell' }
+                            }}>Position</TableCell>
+                        <TableCell sx={{
+                            border: "none", 
+                            fontWeight: "700",
+                            display: { xs: 'none', sm: 'table-cell' }
+                            }}>Department</TableCell>
+                        <TableCell sx={{
+                            border: "none", 
+                            fontWeight: "700",
+                            display: { xs: 'none', sm: 'table-cell' }
+                            }}>Cell</TableCell>
+                        <TableCell sx={{
+                            border: "none", 
+                            fontWeight: "700", 
+                            display: { xs: 'none', sm: 'table-cell' }
+                            }}>Email</TableCell>
+                        <TableCell sx={{border: "none", fontWeight: "700"}}>Action</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    <TableRow sx={{
+                        backgroundColor: "#f9f9f9",
+                        margin: "5px",
+                    }}>
+                        <TableCell component="th" sx={{border: "none", fontWeight: "500"}}>John Doe</TableCell>
+                        <TableCell sx={{
+                            border: "none", 
+                            fontWeight: "500",
+                            display: { xs: 'none', sm: 'table-cell' }
+                            }}>BB Electric</TableCell>
+                        <TableCell sx={
+                            {border: "none", 
+                            fontWeight: "500",
+                            display: { xs: 'none', sm: 'table-cell' }
+                            }}>G&#38;E</TableCell>
+                        <TableCell sx={{
+                            border: "none", 
+                            fontWeight: "500",
+                            display: { xs: 'none', sm: 'table-cell' }
+                            }}>407-404-2878</TableCell>
+                        <TableCell sx={{
+                            border: "none", 
+                            fontWeight: "500",
+                            display: { xs: 'none', sm: 'table-cell' }
+                            }}>john.doe@gmail.com</TableCell>
+                        <TableCell sx={{border: "none", fontWeight: "500"}}>View</TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+        </TableContainer>
+            
+        </div>
     )
 }
 
