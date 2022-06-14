75e64503d7e2db1aa9421fa7e4bebf69fa2be3f7
marcivallario <68357009+marcivallario@users.noreply.github.com>
2022-06-14 00:22:39 -0400

    Heroku client side routing settings update

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
-  resources :accommodations
-  resources :transportations
-  resources :flights
-  resources :ffnumbers
-  resources :trips
-  resources :passengers
-  resources :projects
-  get '/auth', to: 'users#show'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-  post '/signup', to: 'users#create'
+  namespace :api do
+    resources :accommodations
+    resources :transportations
+    resources :flights
+    resources :ffnumbers
+    resources :trips
+    resources :passengers
+    resources :projects
+    get '/auth', to: 'users#show'
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
+    post '/signup', to: 'users#create'
+  end
+  
   get '*path',
       to: 'fallback#index',
       constraints: ->(req) { !req.xhr? && req.format.html? }
