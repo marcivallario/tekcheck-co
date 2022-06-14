e5bb4bb582597c23fc1054955053179946c6dc94
marcivallario <68357009+marcivallario@users.noreply.github.com>
2022-06-14 01:13:49 -0400

    Update readme and fix routes

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# TrekCheck Co
+# TrekCheck
+
+TrekCheck is an app for tracking trip lodging, transportation, & accommodation arrangements and storing traveler profiles.
+
+## Motivation
+The inspiration for TrekCheck came when working as a travel coordinator in film & television. The pure amount of data was overwhelming and all was tracked via a giant Google Sheet—-I cataloged features for an app I could build that would separate and handle this data to only serve me what I needed.
+
+## Project Video
+Coming soon.
+
+## Tech / Frameworks Used
+
+### Backend Development
+
+Ruby
+Ruby on Rails - version 7.0
+
+### Frontend Development
+
+JavaScript (ES6)
+HTML
+CSS
+React.js - version 17.0.2
+React-DOM - version 17.0.2
+React-Router-DOM - version 5.3.0
+Material UI - version 5.5.3
+Material Icons - version 5.6.1
+Alice Carousel - version 2.5.1
+React Redux - version 7.2.8
+Reduxjs Toolkit - version 1.8.1
+
+## Setup
+To try out this project:
+
+- Clone the GitHub repository locally to your computer
+- In the command line, navigate to the root directory of the repository, and type the following: ```$ npm install```
+- Then type the following: $ ```npm install —prefix client```
+- The type: ```npm start —prefix client```
+- Lastly, start the server: ```rails s```
+
+## Status
+This project is still in development—stay tuned for updates.
+
+## License
+[GNU](https://choosealicense.com/licenses/gpl-3.0/)
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,35 @@
-Rails.application.routes.draw do
-  namespace :api do
-    resources :accommodations
-    resources :transportations
-    resources :flights
-    resources :ffnumbers
-    resources :trips
-    resources :passengers
-    resources :projects
-    get '/auth', to: 'users#show'
-    post '/login', to: 'sessions#create'
-    delete '/logout', to: 'sessions#destroy'
-    post '/signup', to: 'users#create'
-  end
+# Rails.application.routes.draw do
+#   namespace :api do
+#     resources :accommodations
+#     resources :transportations
+#     resources :flights
+#     resources :ffnumbers
+#     resources :trips
+#     resources :passengers
+#     resources :projects
+#     get '/auth', to: 'users#show'
+#     post '/login', to: 'sessions#create'
+#     delete '/logout', to: 'sessions#destroy'
+#     post '/signup', to: 'users#create'
+#   end
   
+#   get '*path',
+#       to: 'fallback#index',
+#       constraints: ->(req) { !req.xhr? && req.format.html? }
+# end
+
+Rails.application.routes.draw do
+  resources :accommodations
+  resources :transportations
+  resources :flights
+  resources :ffnumbers
+  resources :trips
+  resources :passengers
+  resources :projects
+  get '/auth', to: 'users#show'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  post '/signup', to: 'users#create'
   get '*path',
       to: 'fallback#index',
       constraints: ->(req) { !req.xhr? && req.format.html? }
